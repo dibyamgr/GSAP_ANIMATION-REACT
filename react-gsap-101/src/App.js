07814b5fc4c56ea1a81735f1af78f3a479d5fd6e
dibyamgr <dibyamagar5@gmail.com>
2022-08-19 21:06:29 +0545

    GSAP Animation when state changes

--- react-gsap-101/src/App.js
+++ react-gsap-101/src/App.js
@@ -1,33 +1,52 @@
-import React, { useRef, useLayoutEffect } from "react";
+import React, { useRef, useLayoutEffect, useState, useEffect } from "react";
 import logo from './logo.svg';
 import './App.css';
 import { gsap } from 'gsap';
 
 function App() {
+  // state for backfround
+  const [background, setBackground] = useState("#262626")
+
   // to target the header , default -> null
   const headerRef = useRef(null);
 
-  useLayoutEffect(() => {
-    let from = gsap
-      .from(headerRef.current, {
-        duration: 1,
-        autoAlpha: 0,
-        ease: 'none',
-        delay: 1,
-        immediateRender: false
-      })
-    return () => {
-      from.kill()
-    }
-  })
-  // set header Ref as dependency
+  // useLayoutEffect(() => {
+  //   let from = gsap
+  //     .from(headerRef.current, {
+  //       immediateRender: false,
+  //       duration: 1,
+  //       autoAlpha: 0,
+  //       ease: 'none',
+  //       delay: 1,
+  //     })
+  //   return () => {
+  //     from.kill()
+  //   }
+  // })
+
+  // animate when state changes
+  const toggleBackground = () => {
+    const color = background !== '#262626' ? "#5a7d95" : "#1b4943"
+    setBackground(color)
+  }
+
+  useEffect(() => {
+    gsap.to(headerRef.current, {
+      immediateRender: false,
+      duration: 1,
+      backgroundColor: background
+
+    })
+  }, [background])
+
 
   return (
     <div className="App">
       {/* fade in header when page loads - we need to target the header DOM Element
-      and twig the opacity  */}
+      and tween the opacity  */}
       <header ref={headerRef} className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={() => toggleBackground()}>Toggle Background</button>
         <p>
           Scroll down to see sections being revealed by ScrollTrigger
         </p>
